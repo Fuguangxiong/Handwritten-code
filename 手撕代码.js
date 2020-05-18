9998b325f8a499208dadb1ccaa4077df43cbb658
Fuguangxiong <fgx583522728@163.com>
2020-05-18 22:19:16 +0800

    加入JavaScript深入之创建对象的多种方式以及优缺点

--- 手撕代码.js
+++ 手撕代码.js
@@ -193,7 +193,7 @@ let b = new SubType()
 //无论什么情况下，都会调用两次超类型构造函数：一次是在创建子类型原型的时候，另一次是在子类型构造函数内部。
 //原型式集成
 function object(o) {
-    function F() { }
+    function F() {}
 
     F.prototype = o
     return new F()
@@ -204,7 +204,7 @@ function object(o) {
 //在传入一个参数的情况下，Object.create() 和 object() 方法的行为相同。
 //寄生式集成
 function object(o) {
-    function F() { }
+    function F() {}
 
     F.prototype = o
     return new F()
@@ -212,7 +212,7 @@ function object(o) {
 
 function createAnother(original) {
     let clone = object(original)
-    clone.sayHi = function () { }
+    clone.sayHi = function () {}
     return clone
 }
 
@@ -460,55 +460,144 @@ function instance(left, right) {
     }
 }
 
- //封装jsonp
- function jsonp(url, params, callback, outTime) {
+//封装jsonp
+function jsonp(url, params, callback, outTime) {
     let body = document.body
     let fnName = "_jsonpFn" + Math.random().toString().replace('.', '')
     let script = document.createElement('script')
     window[fnName] = function (data) {
-      callback(data)
-      delete window[fnName]
-      body.removeChild(script)
+        callback(data)
+        delete window[fnName]
+        body.removeChild(script)
     }
     let str = ''
     for (const key of params) {
-      str += key + "=" + params[key] + "&"
+        str += key + "=" + params[key] + "&"
     }
     str += 'callback=' + fnName
     script.src = url + '?' + str
     body.insertBefore(script, document.body.firstChild)
     if (outTime) {
-      let timer = window.setTimeout(() => {
-        body.removeChild(script)
-        clearTimeout(timer)
-      }, outTime)
+        let timer = window.setTimeout(() => {
+            body.removeChild(script)
+            clearTimeout(timer)
+        }, outTime)
     }
-  }
+}
 
-  function jsonpromise(url) {
+function jsonpromise(url) {
     let json;
     let s = document.createElement('script');
     s.src = url + '?callback=fn';
     window.fn = function (data) {
-      json = data;
+        json = data;
     }
     //当script被插入文档中时，src中的资源就会开始加载
     document.body.appendChild(s);
 
     return new Promise((resolve, reject) => {
-      /* throw('err in promise'); */
-      s.onload = function (e) {
-        resolve(json);
-      }
-      s.onerror = function () {
-        reject(json);
-      }
+        /* throw('err in promise'); */
+        s.onload = function (e) {
+            resolve(json);
+        }
+        s.onerror = function () {
+            reject(json);
+        }
     });
-  }
-  jsonpromise('http://localhost:8082').then(data => {
+}
+jsonpromise('http://localhost:8082').then(data => {
     console.log(data);
     throw ('err before then');
-  }).catch(err => {
+}).catch(err => {
     //可以捕捉到then里的err befor then也可以捕捉到new Promise里的err in promise。
     console.log(err)
-  })
+})
+
+//JavaScript深入之创建对象的多种方式以及优缺点
+//1.工厂模式
+function createPerson(name) {
+    let o = new Object();
+    o.name = name
+    o.getName = function () {
+        console.log(this.name)
+    }
+    return o
+}
+// 缺点：对象无法识别，因为所有的实例都指向一个原型
+//2. 构造函数模式
+function Person(name) {
+    this.name = name;
+    this.getName = function () {
+        console.log(this.name);
+    };
+}
+var person1 = new Person('kevin');
+// 优点： 实例可以识别为一个特定的类型
+// 缺点： 每次创建实例时， 每个方法都要被创建一次
+//2.1 构造函数模式优化
+function Person(name) {
+    this.name = name;
+    this.getName = getName;
+}
+
+function getName() {
+    console.log(this.name);
+}
+var person1 = new Person('kevin');
+// 优点： 解决了每个方法都要被重新创建的问题
+// 缺点： 这叫啥封装……
+// 3. 原型模式
+function Person(name) {
+
+}
+Person.prototype.name = 'keivn';
+Person.prototype.getName = function () {
+    console.log(this.name);
+};
+var person1 = new Person();
+// 优点： 方法不会重新创建
+// 缺点： 1. 所有的属性和方法都共享 2. 不能初始化参数
+//3.1 原型模式优化
+function Person(name) {
+
+}
+Person.prototype = {
+    name: 'kevin',
+    getName: function () {
+        console.log(this.name);
+    }
+};
+var person1 = new Person();
+// 优点： 封装性好了一点
+// 缺点： 重写了原型， 丢失了constructor属性
+// 3.2 原型模式优化
+
+function Person(name) {
+
+}
+
+Person.prototype = {
+    constructor: Person,
+    name: 'kevin',
+    getName: function () {
+        console.log(this.name);
+    }
+};
+
+var person1 = new Person();
+// 优点： 实例可以通过constructor属性找到所属构造函数
+// 缺点： 原型模式该有的缺点还是有
+// 4. 组合模式
+// 构造函数模式与原型模式双剑合璧。
+function Person(name) {
+    this.name = name;
+}
+Person.prototype = {
+    constructor: Person,
+    getName: function () {
+        console.log(this.name);
+    }
+};
+// var person1 = new Person();
+// 优点： 该共享的共享， 该私有的私有， 使用最广泛的方式
+// 缺点： 有的人就是希望全部都写在一起， 即更好的封装性
