7ed44cf71f429ec3a27f63bf62ea51d61f379712
curryff <curryff@yeahka.com>
2020-05-18 18:33:23 +0800

    算法和手撕代码分类

---
+++ 数组.js
@@ -0,0 +1,170 @@
+//冒泡排序最终优化版
+function bubble(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      let Change = false
+      for (let j = 0; j < arr.length - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          Change = true
+          arr[j] = arr[j] ^ arr[j + 1]
+          arr[j + 1] = arr[j] ^ arr[j + 1]
+          arr[j] = arr[j + 1] ^ arr[j]
+        }
+      }
+      if (!Change) {
+        return arr
+      }
+    }
+  }
+
+  // console.log(bubble([1, 5, 9, 7, 5, 3, 2, 4]))
+  //选择排序
+  function selectSort(arr) {
+    let minIndex, temp
+    for (let i = 0; i < arr.length; i++) {
+      minIndex = i //假设第一项是值最小的索引
+      for (let j = i; j < arr.length; j++) {
+        if (arr[j] < arr[minIndex]) {
+          minIndex = j
+        }
+      }
+      temp = arr[minIndex]
+      arr[minIndex] = arr[i]
+      arr[i] = temp
+    }
+    return arr
+  }
+
+  // console.log(selectSort([1, 5, 9, 7, 5, 3, 2, 4]))
+  //插入排序
+  function insertSort(arr) {
+    if (arr.length < 2) {
+      return arr
+    }
+    let temp, j
+    for (let i = 1; i < arr.length; i++) {
+      j = i
+      temp = arr[i]
+      // while (j > 0 && arr[j - 1] > temp) {
+      //     arr[j] = arr[j - 1]
+      //     j--
+      // }
+      for (let k = i - 1; k < i && k > -1; k--) {
+        if (temp < arr[k]) {
+          arr[k + 1] = arr[k]
+          arr[k] = temp
+        }
+      }
+    }
+    return arr
+  }
+
+  //希尔排序 (对插入排序的升级)
+  var shellSort = function () {
+    if (this.array === null || this.array.length < 2) return this.array
+    let length = this.array.length
+    //初始化增量
+    var gap = Math.floor(length / 2)
+    // whlie循环
+    while (gap > 1) {
+      for (let i = gap; i < length; i++) {
+        let temp = this.array[i]
+        let j = i
+        while (this.array[j - gap] > temp && j > gap - 1) {
+          this.array[j] = this.array[j - gap]
+          j -= gap
+        }
+        this.array[j] = temp
+      }
+      gap = Math.floor(gap / 2)
+    }
+  }
+
+  // console.log(insertSort([1, 5, 9, 7, 5, 3, 2, 4]))
+  //快速排序
+  //方法一优化版
+  // function quickSort(arr, left, right) { //这个left和right代表分区后“新数组”的区间下标，因为这里没有新开数组，所以需要left/right来确认新数组的位置
+  //     if (left < right) {
+  //         let pos = left - 1 //pos即“被置换的位置”，第一趟为-1
+  //         for (let i = left; i <= right; i++) { //循环遍历数组，置换元素
+  //             let pivot = arr[right] //选取数组最后一位作为基准数，
+  //             if (arr[i] <= pivot) { //若小于等于基准数，pos++，并置换元素, 这里使用小于等于而不是小于, 其实是为了避免因为重复数据而进入死循环
+  //                 pos++
+  //                 let temp = arr[pos]
+  //                 arr[pos] = arr[i]
+  //                 arr[i] = temp
+  //             }
+  //         }
+  //         //一趟排序完成后，pos位置即基准数的位置，以pos的位置分割数组
+  //         quickSort(arr, left, pos - 1)
+  //         quickSort(arr, pos + 1, right)
+  //     }
+  //     return arr //数组只包含1或0个元素时(即left>=right)，递归终止
+  // }
+  // //使用
+  // var arr = [5, 1, 4, 2, 3]
+  // var start = 0;
+  // var end = arr.length - 1;
+  // quickSort(arr, start, end)
+  //方法二普通版
+  // function quickSort(arr) {
+  //   if (arr.length < 2) {
+  //     return arr
+  //   }
+  //   let end = arr.length - 1
+  //   let pivot = arr.splice(Math.floor(end / 2), 1)[0];
+  //   // let base = arr[Math.floor(end / 2)]
+  //   let leftArr = []
+  //   let rightArr = []
+  //   for (let i = 0; i < arr.length; i++) {
+  //     if (arr[i] <= pivot) {
+  //       leftArr.push(arr[i])
+  //     } else {
+  //       rightArr.push(arr[i])
+  //     }
+  //   }
+  //   return quickSort(leftArr).concat([pivot], quickSort(rightArr))
+  // }
+  //方法三while版
+  // var arr = [3, 3, -5, 6, 0, 2, -1, -1, 3];
+  // console.log(arr);
+  // quick_sort(arr, 0, arr.length - 1);
+  // console.log(arr);
+  /**
+  题目：快速排序算法
+  思路：两个哨兵，i,j,j从右边找比基数小的，i从左边找比基数大的，然后交换两个目标元素的位置，直到i=j,然后交换i和基数的位置，递归处理。
+  **/
+  function quickSort(arr, from, to) {
+    var i = from; //哨兵i
+    var j = to; //哨兵j
+    var key = arr[from]; //标准值
+    if (from >= to) { //如果数组只有一个元素
+      return;
+    }
+    while (i < j) {
+      while (arr[j] > key && i < j) { //从右边向左找第一个比key小的数，找到或者两个哨兵相碰，跳出循环
+        j--;
+      }
+      while (arr[i] <= key && i <
+        j) { //从左边向右找第一个比key大的数，找到或者两个哨兵相碰，跳出循环,这里的=号保证在本轮循环结束前，key的位置不变，否则的话跳出循环，交换i和from的位置的时候，from位置的上元素有可能不是key
+        i++;
+      }
+      /**
+        代码执行道这里，1、两个哨兵到找到了目标值。2、j哨兵找到了目标值。3、两个哨兵都没找到(key是当前数组最小值)
+      **/
+      if (i < j) { //交换两个元素的位置
+        var temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+      }
+    }
+    arr[from] = arr[i] //将基数与下标为 i 的元素原地交换，从而实现划分；
+    arr[i] = key;
+    quickSort(arr, from, i - 1);
+    quickSort(arr, i + 1, to);
+  }
+
+  // arr = [2, 1, 5, 8, 3, 7, 4, 6, 9];
+  // console.log(quickSort(arr)); //[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+  
---
+++ 链表.js
@@ -0,0 +1,326 @@
+ //实现一个链表
+ function LinkList() {
+    function Node(value) {
+      this.value = value
+      this.next = null
+    }
+
+    this.head = null
+    this.length = 0
+    LinkList.prototype.append = function (data) {
+      if (this.head === null) {
+        this.head = new Node(data)
+      } else {
+        let current = this.head
+        while (current.next) {
+          current = current.next
+        }
+        current.next = new Node(data)
+      }
+      this.length++
+    }
+    LinkList.prototype.find = function (data) {
+      let current = this.head
+      do {
+        if (current.value === data) {
+          return current
+        }
+        current = current.next
+      } while (current);
+    }
+    LinkList.prototype.fixed = function (data, newdata) {
+      let current = this.find(data)
+      current.data = newdata
+    }
+    LinkList.prototype.prefind = function (data) { }
+    LinkList.prototype.delete = function (data) {
+      if (data === this.head.value) {
+        this.head = this.head.next
+      } else {
+        let current = this.head
+        while (current) {
+          if (current.next.value === data) {
+            current.next = current.next.next
+            break
+          }
+          current = current.next
+        }
+      }
+    }
+    LinkList.prototype.toString = function () {
+      let result = ''
+      let current = this.head
+      if (this.head === null) {
+        return result
+      } else {
+        while (current) {
+          result += current.value + '=>'
+          current = current.next
+        }
+        result = result + 'null'
+        return result
+      }
+    }
+  }
+
+  let link = new LinkList()
+  link.append(1)
+  link.append(3)
+  link.append('a')
+  link.append(6)
+  link.append('b')
+  link.append('7')
+  link.append(1)
+  // link.delete('a')
+  // link.delete(1)
+  // link.delete(1)
+  console.log(link.toString(), '链表的打印')
+  console.log(link.find('a'), '链表的查找')
+
+  // console.log(intersection([4, 9, 5], [9, 4, 9, 8, 4], [8, 4]))
+  //合并两个有序链表
+  // 将两个升序链表合并为一个新的升序链表并返回。 新链表是通过拼接给定的两个链表的所有节点组成的。
+  // 示例：
+  // 输入： 1 - > 2 - > 4, 1 - > 3 - > 4
+  // 输出： 1 - > 1 - > 2 - > 3 - > 4 - > 4
+  function mergeTwoLists(l1, l2) {
+    if (l1 === null) {
+      return l2
+    }
+    if (l2 === null) {
+      return l1
+    }
+    if (l1.val <= l2.val) {
+      l1.next = mergeTwoLists(l1.next, l2)
+      return l1
+    } else {
+      l2.next = mergeTwoLists(l2.next, l1)
+      return l2
+    }
+  }
+
+  //判断一个单链表是否有环
+  //标记法
+  // let hasCycle = function (head) {
+  //     while (head) {
+  //         if (head.flag) return true
+  //         head.flag = true
+  //         head = head.next
+  //     }
+  //     return false
+  // };
+  //利用 JSON.stringify() 不能序列化含有循环引用的结构
+  // let hasCycle = function (head) {
+  //     let flag = false
+  //     try {
+  //         JSON.stringify(head)
+  //     } catch (error) {
+  //         flag = true
+  //     }
+  //     return flag
+  // };
+  // 解法三： 快慢指针（ 双指针法）
+  // 设置快慢两个指针， 遍历单链表， 快指针一次走两步， 慢指针一次走一步， 如果单链表中存在环， 则快慢指针终会指向同一个节点， 否则直到快指针指向 null 时， 快慢指针都不可能相遇
+  let hasCycle = function (head) {
+    if (!head || !head.next) {
+      return false
+    }
+    let fast = head.next.next,
+      slow = head.next
+    while (fast !== slow) {
+      if (!fast || !fast.next) return false
+      fast = fast.next.next
+      slow = slow.next
+    }
+    return true
+  };
+  //反转链表
+  // 输入: 1 - > 2 - > 3 - > 4 - > 5 - > NULL
+  // 输出: 5 - > 4 - > 3 - > 2 - > 1 - > NULL
+  // var reverseList = function (head) {
+  //     if (!head || !head.next) return head
+  //     var next = head.next
+  //     // 递归反转
+  //     var reverseHead = reverseList(next)
+  //     // 变更指针
+  //     next.next = head
+  //     head.next = null
+  //     return reverseHead
+  // };
+  var reverseList = function (head) {
+    let prev = null;
+    let curr = head;
+    while (curr != null) {
+      let next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  };
+  // var reverseList = function (head) {
+  //     let reverse = (prev, curr) => {
+  //         if (!curr) return prev;
+  //         let next = curr.next;
+  //         curr.next = prev;
+  //         return reverse(curr, next);
+  //     }
+  //     return reverse(null, head);
+  // };
+  // console.log(reverseList(link).toString(), '链表的反转')
+  //求链表的中间结点
+  //遍历将节点放在数组中， 然后取中间值
+  var middleNode = function (head) {
+    if (!head) return []
+    var arr = []
+    while (head) {
+      arr.push(head)
+      head = head.next
+    }
+    return arr[Math.ceil((arr.length - 1) / 2)]
+  };
+  // 利用双指针，快指针走两步，慢指针走一步，快指针走完，慢指针则为中间值
+  // const getMiddleNode = function (head) {
+  // if (!head) return null;
+  // let fast = head.next.next,
+  // slow = head.next;
+  // while (fast && fast.next) {
+  // fast = fast.next.next;
+  // slow = slow.next;
+  // }
+  // return slow;
+  // };
+  //删除链表倒数第 n 个结点
+  // 这道题要用双指针来实现。先用first指针前进n，然后让second从head开始和first一起前进，直到first到了末尾，
+  // 此时second的下一个节点就是要删除的节点。（另外，若first一开始前进n就已经不在链表中了，说明要删除的节点正是head节点，
+  // 那么直接返回head的下一个节点接口。）
+  var removeNthFromEnd = function (head, n) {
+    let first = head,
+      second = head;
+    while (n > 0) {
+      first = first.next
+      n--
+    }
+    if (!first) return head.next; // 删除的是头节点
+    while (first.next) {
+      first = first.next;
+      second = second.next;
+    }
+    second.next = second.next.next;
+    return head
+  };
+  //编写一个程序，找到两个单链表相交的起始节点
+  // 两次遍历， 先遍历一个链表， 给链表中的每个节点都增加一个标志位， 然后遍历另外一个链表， 遍历到第一个已被标志过的节点为两链表相交的起始节点。
+  // 若遍历完都没有发现已被标志过的节点， 则两链表不相交， 返回 null
+  var getIntersectionNode = function (headA, headB) {
+    while (headA) {
+      headA.flag = true
+      headA = headA.next
+    }
+    while (headB) {
+      if (headB.flag) return headB
+      headB = headB.next
+    }
+    return null
+  };
+  //双指针法
+  // var getIntersectionNode = function (headA, headB) {
+  //     // 清除高度差
+  //     let pA = headA,
+  //         pB = headB
+  //     while (pA || pB) {
+  //         if (pA === pB) return pA
+  //         pA = pA === null ? headB : pA.next
+  //         pB = pB === null ? headA : pB.next
+  //     }
+  //     return null
+  // };
+
+   //判断一个链表是否为回文链表
+   var isPalindrome = function (head) {
+    if (head === null || head.next === null) return true;
+    let mid = head;
+    let pre = null;
+    let reversed = null;
+    // end每次走两格，这个循环的时间复杂度为O(n/2)
+    while (head !== null && head.next !== null) {
+      // 这个赋值要在mid被修改前提前
+      pre = mid
+      // 遍历链表
+      mid = mid.next
+      head = head.next.next
+      // 反转前面部分的节点，并用reversed保存
+      pre.next = reversed
+      reversed = pre
+    }
+    // 奇数mid往后走一位
+    if (head) mid = mid.next
+    while (mid) {
+      if (reversed.val !== mid.val) return false
+      reversed = reversed.next
+      mid = mid.next
+    }
+    return true
+  };
+
+  //排序链表
+  var sortList = function (head) {
+    function ListNode(val) {
+      this.next = null
+      this.val = val
+    }
+    // function merge(left, right) {  //方法1:合并两个有序链表
+    //   let res = new ListNode(0);
+    //   let pre = res;   //因为pre一直跟着链表移动所以需要 用一个res记录链表的起点
+    //   while (left && right) {
+    //     if (left.val <= right.val) {
+    //       pre.next = left;
+    //       left = left.next;
+    //     } else {
+    //       pre.next = right;
+    //       right = right.next;
+    //     }
+    //     pre = pre.next;
+    //   }
+    //   pre.next = left ? left : right;
+    //   return res.next;
+    // }
+
+    function merge(l1, l2) {   //方法2:合并两个有序链表
+      if (l1 === null) {
+        return l2
+      }
+      if (l2 === null) {
+        return l1
+      }
+      if (l1.val <= l2.val) {
+        l1.next = merge(l1.next, l2)
+        return l1
+      }
+      if (l1.val > l2.val) {
+        l2.next = merge(l1, l2.next)
+        return l2
+      }
+    }
+    function mergeSort(head) {
+      if (!head || !head.next) {
+        return head
+      }
+      let left //链表的中点前一部分
+      let right //链表中点后一部分
+      //  let mid //中间点
+      //慢指针走一步  快指针走两步  快指针到达中点时 慢指针的位置就是我们认为的中点
+      let fast = head //开始快慢指针均位于起点  
+      let slow = head
+      while (fast.next && fast.next.next) {
+        slow = slow.next
+        fast = fast.next.next
+      }
+      //slow即是中间点
+      right = slow.next
+      slow.next = null
+      left = head  //不能写left = slow因为slow一直在移动
+      return merge(mergeSort(left), mergeSort(right))
+    }
+    return mergeSort(head)
+  }
